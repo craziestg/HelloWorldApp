8b56b47e474adaa7715b0041b76fd71f9169b6e9
Guillermo Gimenez <gimenez.g91@gmail.com>
2014-06-11 00:10:10 -0500

    Started work on the app
    figuring out how to work Xcode and github

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Guillermo Gimenez on 6/11/14.
+//  Copyright (c) 2014 Guillermo Gimenez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
